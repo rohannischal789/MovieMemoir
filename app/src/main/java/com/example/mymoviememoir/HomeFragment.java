a2acd1e483fe315a6b87ac15c1a003679a1db7ea
rohannischal789 <rnis0005@student.monash.edu>
2020-05-31 18:17:39 +1000

    Improvements for home

--- app/src/main/java/com/example/mymoviememoir/HomeFragment.java
+++ app/src/main/java/com/example/mymoviememoir/HomeFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -14,10 +15,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mymoviememoir.adapter.HomeRecycleViewAdapter;
+import com.example.mymoviememoir.entity.Person;
 import com.example.mymoviememoir.model.Movie;
 import com.example.mymoviememoir.networkconnection.NetworkConnection;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
@@ -27,9 +31,14 @@ public class HomeFragment extends Fragment {
     private List<Movie> movies;
     private HomeRecycleViewAdapter adapter;
     private NetworkConnection networkConnection;
+    private TextView tvPersonName;
+    private TextView tvDate;
+    private TextView emptyView;
+
     public HomeFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -38,16 +47,36 @@ public class HomeFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerViewTop5);
         networkConnection = new NetworkConnection();
         movies = new ArrayList<Movie>();
+        tvPersonName = view.findViewById(R.id.tvPersonName);
+        tvDate = view.findViewById(R.id.tvDate);
+        emptyView = view.findViewById(R.id.empty_view);
 
+        tvDate.setText("Today's Date: " + new SimpleDateFormat("dd-MM-yyyy").format(new Date()));
         SharedPreferences sharedPref = getActivity().getSharedPreferences("data", Context.MODE_PRIVATE);
-        int personID = sharedPref.getInt("personID",0);
-        findMovieByName movieByName = new findMovieByName();
-        movieByName.execute(personID);
+        int personID = sharedPref.getInt("personID", 0);
+
+        GetPerson getPerson = new GetPerson();
+        getPerson.execute(personID);
 
         return view;
     }
 
-    private class findMovieByName extends AsyncTask<Integer, Void, List<Movie>> {
+    private class GetPerson extends AsyncTask<Integer, Void, Person> {
+
+        @Override
+        protected Person doInBackground(Integer... params) {
+            return networkConnection.getPersonByID(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Person details) {
+            tvPersonName.setText("Hello " + details.getFirstname());
+            FindTop5UserRatedMovies movies = new FindTop5UserRatedMovies();
+            movies.execute(details.getPersonid());
+        }
+    }
+
+    private class FindTop5UserRatedMovies extends AsyncTask<Integer, Void, List<Movie>> {
 
         @Override
         protected List<Movie> doInBackground(Integer... params) {
@@ -57,11 +86,18 @@ public class HomeFragment extends Fragment {
         @Override
         protected void onPostExecute(List<Movie> details) {
 
-            adapter = new HomeRecycleViewAdapter(details);
-            recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
-            recyclerView.setAdapter(adapter);
-            layoutManager = new LinearLayoutManager(getActivity());
-            recyclerView.setLayoutManager(layoutManager);
+            if (details.size() > 0) {
+                recyclerView.setVisibility(View.VISIBLE);
+                emptyView.setVisibility(View.GONE);
+                adapter = new HomeRecycleViewAdapter(details);
+                recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+                recyclerView.setAdapter(adapter);
+                layoutManager = new LinearLayoutManager(getActivity());
+                recyclerView.setLayoutManager(layoutManager);
+            } else {
+                recyclerView.setVisibility(View.GONE);
+                emptyView.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
